33e7ce47e052fc39ea9ba551275e2b2028ead356
EvATive7 <evative7@gmail.com>
2024-08-04 22:10:02 +0800

    chore

--- main.py
+++ main.py
@@ -206,4 +206,4 @@ def get_danmu(cid, segment_index):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=14786)
